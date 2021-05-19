d7b62716b02e61d417b96373a627ea4a36bbeffd
justinait <justiiturraspe@gmail.com>
2021-05-18 22:24:10 -0300

    app

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const path = require('path')
 const app = express();
+const port = process.env.PORT || 3000;
 
 const publicPath = path.resolve(__dirname, './public')
 app.use(express.static(publicPath))
 
-app.listen(3000, ()=> console.log('servidor corriendo'))
+app.listen(port, ()=> console.log('servidor corriendo en http://localhost:${port}'))
 
 app.get('/', (req, res)=>{
     res.sendFile(path.resolve(__dirname, './views/home.html'))
