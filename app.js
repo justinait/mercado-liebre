e73776e7f63e5dff96d4915d08d9b9437176dd19
justinait <justiiturraspe@gmail.com>
2021-05-18 22:59:56 -0300

    app

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
-const path = require('path')
-const app = express();
-const port = process.env.PORT || 3000;
 
+const app = express();
+const port = process.env.PORT || 3030;
+
+const path = require('path')
 const publicPath = path.resolve(__dirname, './public')
+
 app.use(express.static(publicPath))
 
 app.listen(port, ()=> console.log('servidor corriendo en http://localhost:${port}'))
